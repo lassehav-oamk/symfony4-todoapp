5f2b08c243cb0c30bbed7436a1563fd29e084565
Lasse Haverinen <lasse.haverinen@gmail.com>
2018-08-14 11:02:52 +0300

    Added simple twig based rendering of todos from static data passed by controller

---
+++ symfonyApp/src/Controller/MainController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController
+{
+    public function todoList()
+    {
+        $todos = array(
+            array('id' => 1,
+                  'description' => 'Get apples',
+                  'dueDate' => '2001-03-10',
+                  'isDone' => true),
+            array('id' => 2,
+                'description' => 'Learn Symfony',
+                'dueDate' => '2018-09-20',
+                'isDone' => false),
+            array('id' => 3,
+                'description' => 'Buy milk',
+                'dueDate' => '2018-10-01',
+                'isDone' => false),
+            );
+
+        return $this->render('todoList.html.twig', array(
+            'todos' => $todos,
+        ));
+    }
+}
