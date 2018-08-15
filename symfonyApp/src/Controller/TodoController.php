a4174c0acbfc42662f6634e02602d0869226f534
Lasse Haverinen <lasse.haverinen@gmail.com>
2018-08-15 14:55:51 +0300

    Implemented form data submit and saving

--- symfonyApp/src/Controller/TodoController.php
+++ symfonyApp/src/Controller/TodoController.php
@@ -4,15 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Todo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class TodoController extends AbstractController
 {
-    public function viewTodo($id)
+    public function viewTodo(Request $request, $id)
     {
         $todo = $this->getDoctrine()->getManager()->getRepository(Todo::class)->find($id);
 
@@ -28,9 +28,32 @@ class TodoController extends AbstractController
             ->add('delete', SubmitType::class, array('label' => 'Delete'))
             ->getForm();
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $todo = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($todo);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('todoUpdateSuccess', ['id' => $id]);
+        }
+
         return $this->render('todo/viewTodo.html.twig', [
             'todo' => $todo,
             'form' => $form->createView()
         ]);
+    }
+
+    public function todoUpdated($id)
+    {
+        $todo = $this->getDoctrine()->getManager()->getRepository(Todo::class)->find($id);
+
+        return $this->render('todo/todoUpdated.html.twig', ['todo' => $todo]);
     }
 }
