3a4a055b85701015462ec9bb32ffd284add9f6d4
Lasse Haverinen <lasse.haverinen@gmail.com>
2018-08-20 13:40:17 +0300

    Refactored MainController away

--- symfonyApp/src/Controller/MainController.php
+++ symfonyApp/src/Controller/MainController.php
@@ -9,19 +9,5 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class MainController extends AbstractController
 {
-    public function todoList()
-    {
-        $todos = $this->getDoctrine()
-            ->getRepository(Todo::class)
-            ->findAll();
 
-        $newTodo = new Todo();
-        $newForm = $this->createForm(NewTodo::class, $newTodo, array('action' => $this->generateUrl('todoAddNew')));
-        $newForm->add('save', SubmitType::class);
-
-        return $this->render('todoList.html.twig', array(
-            'todos' => $todos,
-            'newTodoForm' =>  $newForm->createView()
-        ));
-    }
 }
--- symfonyApp/src/Controller/TodoController.php
+++ symfonyApp/src/Controller/TodoController.php
@@ -13,6 +13,22 @@ use App\Form\NewTodo;
 
 class TodoController extends AbstractController
 {
+    public function todoList()
+    {
+        $todos = $this->getDoctrine()
+            ->getRepository(Todo::class)
+            ->findAll();
+
+        $newTodo = new Todo();
+        $newForm = $this->createForm(NewTodo::class, $newTodo, array('action' => $this->generateUrl('todoAddNew')));
+        $newForm->add('save', SubmitType::class);
+
+        return $this->render('todoList.html.twig', array(
+            'todos' => $todos,
+            'newTodoForm' =>  $newForm->createView()
+        ));
+    }
+
     public function viewTodo(Request $request, $id)
     {
         $todo = $this->getDoctrine()->getManager()->getRepository(Todo::class)->find($id);
